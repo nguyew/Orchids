c2c043c4cfc32afa7cd1d1b682d561d4cefa4e27
nguyew <nguyenbaoquan159@gmail.com>
2025-09-19 13:42:54 +0700

    Update OrchidsGrid.jsx

--- src/Orchids/OrchidsGrid.jsx
+++ src/Orchids/OrchidsGrid.jsx
@@ -3,10 +3,12 @@ import OrchidCard from "./OrchidCards";
 
 const OrchidsGrid = ({ orchids, onSelect }) => (
   <Row gutter={[24, 24]} style={{ marginTop: 32 }}>
-    {orchids.map((orchid) => (
+    {orchids.map((orchid, index) => (
       <Col xs={24} sm={12} md={8} lg={6} key={orchid.id}>
-        <div onClick={() => onSelect(orchid)}>
-          <OrchidCard orchid={orchid} />
+        <div style={{ 
+          animationDelay: `${index * 0.1}s`,
+        }}>
+          <OrchidCard orchid={orchid} onSelect={onSelect} />
         </div>
       </Col>
     ))}
